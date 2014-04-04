5a3e1b64e5db0175bb8b35201f703d923fab7241
Carafa, Gianni (SRF) <Gianni.Carafa@srf.ch>
2014-04-04 14:22:31 +0200

    Adding rktask script to run common tasks on host

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -3,4 +3,10 @@ class rkhunter::packages {
     name => "${::rkhunter::params::package_name}",
     ensure => installed,
   }
+
+  file { '/usr/local/bin/rktask':
+    ensure => file,
+    mode   => 755,
+    source => 'puppet:///modules/rkhunter/rktask'
+  }
 }
