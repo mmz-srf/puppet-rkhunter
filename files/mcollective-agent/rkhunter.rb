aabcdc51adcbeb926501215de9c41eb2d320eb89
Carafa, Gianni (SRF) <Gianni.Carafa@srf.ch>
2014-06-26 13:25:04 +0200

    ignore exit code of rkhunter

--- files/mcollective-agent/rkhunter.rb
+++ files/mcollective-agent/rkhunter.rb
@@ -6,15 +6,15 @@ module MCollective
       cat = '/bin/cat'
 
       action 'updatedb' do
-        reply[:status] = run("#{rkhunter} --propupd; #{rkhunter} --cronjob --rwo > /var/log/rkhunter_warnings.log 2>&1 && echo OK || echo ERROR", :stdout => :out, :chomp => true)
+        reply[:status] = run("#{rkhunter} --propupd; #{rkhunter} --cronjob --rwo > /var/log/rkhunter_warnings.log || true", :stdout => :out, :chomp => true)
       end
 
       action 'runcheck' do
-        reply[:status] = run("#{rkhunter} --cronjob --rwo > /var/log/rkhunter_warnings.log 2>&1 && echo OK || echo ERROR", :stdout => :out, :chomp => true)
+        reply[:status] = run("#{rkhunter} --cronjob --rwo > /var/log/rkhunter_warnings.log || true", :stdout => :out, :chomp => true)
       end
 
       action 'showlastrun' do
-        reply[:status] = run("#{cat} /var/log/rkhunter_warnings.log", :stdout => :out, :chomp => true)
+        reply[:status] = run("#{cat} /var/log/rkhunter_warnings.log || true", :stdout => :out, :chomp => true)
       end
 
       activate_when do
