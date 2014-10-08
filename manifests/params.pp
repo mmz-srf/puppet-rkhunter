71c80fb4142211a7019c882ea6d6dd74e714b585
Andreas Zuber <zuber@puzzle.ch>
2014-10-08 14:01:27 +0200

    param defaults for RedHat osfamily from rkhunter-1.4.2-3.el6 RPM (epel)

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,17 +4,35 @@ class rkhunter::params {
   $update_mirrors      = "1"
   $mirrors_mode        = "0"
   $mail_on_warning     = ""
-  $tmpdir              = "/var/lib/rkhunter/tmp"
+  $tmpdir              = $::osfamily ? {
+    'RedHat' => '/var/lib/rkhunter',
+    default  => '/var/lib/rkhunter/tmp',
+  }
   $dbdir               = "/var/lib/rkhunter/db"
   $scriptdir           = "/usr/share/rkhunter/scripts"
-  $logfile             = "/var/log/rkhunter.log"
-  $append_log          = "0"
+  $logfile             = $::osfamily ? {
+    'RedHat' => '/var/log/rkhunter/rkhunter.log',
+    default  => '/var/log/rkhunter.log',
+  }
+  $append_log          = $::osfamily ? {
+    'RedHat' => '1',
+    default  => '0',
+  }
   $copy_log_on_error   = "0"
-  $use_syslog          = undef  #"authpriv.notice"
+  $use_syslog          = $::osfamily ? {
+    'RedHat' => 'authpriv.notice',
+    default  => undef,
+  }
   $color_set2          = "0"
-  $auto_x_detect       = "0"
+  $auto_x_detect       = $::osfamily ? {
+    'RedHat' => '1',
+    default  => '0',
+  }
   $whitelisted_is_white = "0"
-  $allow_ssh_root_user = "no"
+  $allow_ssh_root_user = $::osfamily ? {
+    'RedHat' => 'unset',
+    default  => 'no',
+  }
   $allow_ssh_prot_v1   = "0"
   $enable_tests        = "all"
   $disable_tests       = "suspscan hidden_procs deleted_files packet_cap_apps apps"
@@ -26,18 +44,32 @@ class rkhunter::params {
   $use_locking         = "0"
   $lock_timeout        = "300"
   $show_lockmsgs       = "1"
-  $disable_unhide      = "1"
+  $disable_unhide      = $::osfamily ? {
+    'RedHat' => '0',
+    default  => '1',
+  }
   $installdir          = "/usr"
   $ssh_config_dir      = undef  #"/etc/ssh"
   $hash_func           = undef  #"sha1sum"
   $hash_fld_idx        = undef  #"4"
-  $package_manager     = undef  #"NONE"
+  $package_manager     = $::osfamily ? {
+    'RedHat' => 'RPM',
+    default  => undef,  #"NONE"
+  }
   $pkgmgr_no_verfy     = []     #['/etc/fooconfig1 /etc/barconfig2', '/etc/foobarconfig']
   $ignore_prelink_dep_err = []  #['/bin/ps /usr/bin/top', '/bin/foobin']
   $use_sunsum          = undef  #"0"
-  $existwhitelist      = []     #['/path/one /path/bar', '/path/foobar*']
+  $existwhitelist      = $::osfamily ? {
+    'RedHat' => [
+      '/var/log/pki-ca/system',
+      '/var/log/pki/pki-tomcat/ca/system', # FreeIPA Certificate Authority
+      '/usr/bin/GET',
+      '/usr/bin/whatis',
+    ],
+    default  => [],             #['/path/one /path/bar', '/path/foobar*']
+  }
   $attrwhitelist       = []     #['/path/one /path/bar', '/path/foobar*']
-  $writewhitelist      = []     #['/path/one /path/bar', '/path/foobar*'] 
+  $writewhitelist      = []     #['/path/one /path/bar', '/path/foobar*']
   $scan_mode_dev       = undef  #"THOROUGH"
   $phlanx2_dirtest     = "0"
   $inetd_conf_path     = undef  #"/etc/inetd.conf"
@@ -47,7 +79,13 @@ class rkhunter::params {
   $startup_paths       = []     #['/etc/init.d /etc/rc.local']
   $passwd_file         = undef  #"/etc/shadow"
   $user_fileprop_files_dirs = [] #['top /usr/local/sbin !/opt/ps*', '/etc/rkhunter.conf']
-  $rtkt_file_whitelist = []
+  $rtkt_file_whitelist = $::osfamily ? {
+    'RedHat' => [
+      '/var/log/pki-ca/system',            # FreeIPA Certificate Authority
+      '/var/log/pki/pki-tomcat/ca/system',
+    ],
+    default  => [],
+  }
   $rtkt_dir_whitelist  = []
   $os_version_file     = undef  #"/etc/debian_version"
   $stat_cmd            = undef  #"BUILTIN"
@@ -58,7 +96,16 @@ class rkhunter::params {
   $scanrootkitmode     = undef  #"TROUGH"
   $unhide_tests        = undef  #"sys"
 
-  $scriptwhitelist     = [ 
+  $scriptwhitelist     = $::osfamily ? {
+    'RedHat' => [
+      '/usr/bin/whatis',
+      '/usr/bin/ldd',
+      '/usr/bin/groups',
+      '/usr/bin/GET',
+      '/sbin/ifup',
+      '/sbin/ifdown',
+    ],
+    default  => [
       '/bin/egrep',
       '/bin/fgrep',
       '/bin/which',
@@ -67,17 +114,64 @@ class rkhunter::params {
       '/usr/bin/lwp-request',
       '/usr/sbin/adduser',
       '/usr/sbin/prelink',
-  ]
+    ],
+  }
   $immutewhitelist = [
 #      '/sbin/ifup /sbin/ifdown',
   ]
-  $allowhiddendir = [
+  $allowhiddendir = $::osfamily ? {
+    'RedHat' => [
+      '/etc/.java',
+      '/dev/.udev',
+      '/dev/.udevdb',
+      '/dev/.udev.tdb',
+      '/dev/.udev/db',
+      '/dev/.udev/rules.d',
+      '/dev/.static',
+      '/dev/.initramfs',
+      '/dev/.SRC-unix',
+      '/dev/.mdadm',
+      '/dev/.systemd',
+      '/dev/.mount',
+      '/etc/.git',
+      '/etc/.bzr',
+    ],
+    default  => [
 #      '/etc/.java',
 #      '/dev/.static',
 #      '/dev/.SRC-unix',
 #      '/etc/.etckeeper',
-  ]
-  $allowhiddenfile = [
+    ],
+  }
+  $allowhiddenfile = $::osfamily ? {
+  'RedHat' => [
+    '/usr/share/man/man1/..1.gz',
+    '/lib*/.libcrypto.so.*.hmac',
+    '/lib*/.libssl.so.*.hmac',
+    '/usr/bin/.fipscheck.hmac',
+    '/usr/bin/.ssh.hmac',
+    '/usr/bin/.ssh-keygen.hmac',
+    '/usr/bin/.ssh-keyscan.hmac',
+    '/usr/bin/.ssh-add.hmac',
+    '/usr/bin/.ssh-agent.hmac',
+    '/usr/lib*/.libfipscheck.so.*.hmac',
+    '/usr/lib*/.libgcrypt.so.*.hmac',
+    '/usr/lib*/hmaccalc/sha1hmac.hmac',
+    '/usr/lib*/hmaccalc/sha256hmac.hmac',
+    '/usr/lib*/hmaccalc/sha384hmac.hmac',
+    '/usr/lib*/hmaccalc/sha512hmac.hmac',
+    '/usr/sbin/.sshd.hmac',
+    '/dev/.mdadm.map',
+    '/dev/.udev/queue.bin',
+    '/usr/share/man/man5/.k5login.5.gz',
+    '/usr/share/man/man5/.k5identity.5.gz',
+    '/usr/sbin/.ipsec.hmac',
+    '/sbin/.cryptsetup.hmac',
+    '/etc/.etckeeper',
+    '/etc/.gitignore',
+    '/etc/.bzrignore',
+  ],
+  default  => [
 #      '/etc/.java',
 #      '/usr/share/man/man1/..1.gz',
 #      '/etc/.pwd.lock',
@@ -97,7 +191,8 @@ class rkhunter::params {
 #      '/usr/share/man/man5/.k5login.5.gz',
 #      '/etc/.gitignore',
 #      '/etc/.bzrignore',
-  ]
+    ],
+  }
   $allowprocdelfile = [
 #      '/sbin/cardmgr /usr/sbin/gpm:/etc/X11/abc',
 #      '/usr/lib/libgconf2-4/gconfd-2',
@@ -113,10 +208,25 @@ class rkhunter::params {
   $allowpromiscif = [
 #      'eth0',
   ]
-  $allowdevfile = [
+  $allowdevfile = $::osfamily ? {
+    'RedHat' => [
+      '/dev/shm/pulse-shm-*',
+      '/dev/md/md-device-map',
+      '/dev/shm/mono.*', # tomboy creates this one
+      '/dev/shm/libv4l-*', # created by libv4l
+      '/dev/shm/spice.*', # created by spice video
+      '/dev/.mdadm.map',
+      '/dev/.udev/queue.bin',
+      '/dev/.udev/db/*',
+      '/dev/.udev/rules.d/99-root.rules',
+      '/dev/md/autorebuild.pid', # created by mdadm
+      '/dev/shm/sem.slapd-*.stats', # 389 Directory Server
+    ],
+    default  => [
 #      '/dev/shm/pulse-shm-*',
 #      '/dev/shm/sem.ADBE_*',
-  ]
+    ],
+  }
   $uid0_accounts = [
 #      'toor rooty sashroot',
   ]
